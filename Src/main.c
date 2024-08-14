3ca9c9f02e5f3328f90426d21096619b55b5fec4
angel.in <angel.in.25111977@gmail.com>
2024-08-14 14:57:43 +0300

    battery beeps

--- Src/main.c
+++ Src/main.c
@@ -563,10 +563,12 @@ int main(void) {
       beepCount(4, 24, 1);
     } else if (TEMP_WARNING_ENABLE && board_temp_deg_c >= TEMP_WARNING) {                             // 5 beeps (low pitch): Mainboard temperature warning
       beepCount(5, 24, 1);
-    } else if (BAT_LVL1_ENABLE && batVoltage < BAT_LVL1) {                                            // 1 beep fast (medium pitch): Low bat 1
-      beepCount(0, 10, 6);
+    } else if (BAT_LVL1_ENABLE && batVoltage < BAT_LVL1) {  
+        beepShort(10); beepShort(20); beepShort(30);                                                  // 1 beep fast (medium pitch): Low bat 1
+        //beepCount(0, 10, 6);
     } else if (BAT_LVL2_ENABLE && batVoltage < BAT_LVL2) {                                            // 1 beep slow (medium pitch): Low bat 2
-      beepCount(0, 10, 30);
+        beepShort(10); beepShort(20); beepShort(30);
+        //beepCount(0, 10, 30);
     } else if (BEEPS_BACKWARD && (((cmdR < -50 || cmdL < -50) && speedAvg < 0) || MultipleTapBrake.b_multipleTap)) { // 1 beep fast (high pitch): Backward spinning motors
       beepCount(0, 5, 1);
       backwardDrive = 1;
